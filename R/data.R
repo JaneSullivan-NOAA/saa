fa9460ab9147a619566018ab62c97184b6087e41
Ben Williams <ben.williams@noaa.gov>
2024-03-11 09:04:49 -0800

    init

---
+++ R/data.R
@@ -0,0 +1,32 @@
+#' age specimen data
+#'
+#' A dataset containing age info
+#'
+#' @format A data frame
+#' \describe{
+#' \item{loc}{long_lat}
+#' \item{FMP_SUBAREA}{defined area for location}
+#' }
+"age_data"
+
+#' length specimen data
+#'
+#' A dataset containing length info
+#'
+#' @format A data frame
+#' \describe{
+#' \item{loc}{long_lat}
+#' \item{FMP_SUBAREA}{defined area for location}
+#' }
+"length_data"
+
+#' length bin data
+#'
+#' A dataset containing length_bin info
+#'
+#' @format A data frame
+#' \describe{
+#' \item{loc}{long_lat}
+#' \item{FMP_SUBAREA}{defined area for location}
+#' }
+"len_bins"
---
+++ README.md
@@ -0,0 +1,10 @@
+# size at age
+1. Install package  
+`remotes::install_github('BenWilliams-NOAA\saa')`  
+2. Pull in example data  
+`data("age_data")`  
+`data("length_data")`  
+`data("len_bins")`  
+3. run model  
+`saa(age_data, length_data, len_bins)`  
+4. profit?? 
